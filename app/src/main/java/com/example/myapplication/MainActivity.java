283ea821d7b408c2e846e235a6824cbe9de573c3
a274 <xlnastya@gmail.com>
2019-11-02 09:15:13 +0300

    2.5.2 v2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,12 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txt1 = findViewById(R.id.editText);
-        txt2 = findViewById(R.id.editText2);
-        bt = findViewById(R.id.Button);
+        txt1 = (EditText) findViewById(R.id.editText);
+        txt2 = (EditText) findViewById(R.id.editText2);
+        bt = (Button) findViewById(R.id.Button);
 
 
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String et1 = txt1.getText().toString();
+                String et2 = txt2.getText().toString();
+                if((et1.equals(R.string.login))&&(et2.equals(R.string.password))){
+                    bt.setText(R.string.button_green);
+                    bt.setBackgroundColor(R.color.green1);}
+                else {bt.setText(R.string.button_red);
+                bt.setBackgroundColor(R.color.red1);}
+
+            }
+        });
 
 
     }
-}
+
+        }
