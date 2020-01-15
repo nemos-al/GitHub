7b243c921912b5eaf0bc66e00c6ee2ed659555fb
NeTorch <sladkoDunul>
2020-01-15 14:33:24 +0300

    Draw2D

---
+++ app/src/main/java/ru/samsung/itschool/book/graphic/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.samsung.itschool.book.graphic;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new MyDraw(this));
+    }
+
+}
